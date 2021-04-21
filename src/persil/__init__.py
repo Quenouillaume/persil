0bc8099fbc016afa758b1b7c837bdaf0814a8d11
Guillaume Rousseau <=>
2021-04-21 12:23:04 +0200

    correcting import error

--- src/persil/__init__.py
+++ src/persil/__init__.py
@@ -2,4 +2,4 @@ from .simplexchain import *
 from .homology import *
 from .graphical import *
 
-__all__ = ["persil","simplexchain","homology","graphical"]
+__all__ = ["simplexchain","homology","graphical"]
