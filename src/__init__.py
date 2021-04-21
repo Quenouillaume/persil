fd951616169039cc0fb384f04786481abbe897f3
Guillaume Rousseau <=>
2021-04-21 12:19:44 +0200

    ???????

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-from simplexchain import *
-from homology import *
-from graphical import *
-
-__all__ = ["persil","simplexchain","homology","graphical"]
---
+++ src/persil/__init__.py
@@ -0,0 +1,5 @@
+from .simplexchain import *
+from .homology import *
+from .graphical import *
+
+__all__ = ["persil","simplexchain","homology","graphical"]
