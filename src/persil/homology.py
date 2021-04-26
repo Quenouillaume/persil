928fbcc3d606b9e8cebea872fc784607cac11ea6
Guillaume Rousseau <=>
2021-04-26 20:38:28 +0200

    speedup

--- src/persil/homology.py
+++ src/persil/homology.py
@@ -92,7 +92,7 @@ class ZomorodianCarlsson:
         self.simplices = filteredComplex._simplexes[:]
         self._indexBySimplex = {}
         for i in range(self.n):
-            self._indexBySimplex[self.simplices[i] = i]
+            self._indexBySimplex[self.simplices[i]] = i
         self.dim = filteredComplex._dimension
         self.degrees = filteredComplex._degrees_dict.copy()
         self.field = field
