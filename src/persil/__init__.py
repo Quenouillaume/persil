688c6ecce08ae90d61832a219b7a2c7b44f7605a
Guillaume Rousseau <=>
2021-05-03 00:57:57 +0200

    rips complex

--- src/persil/__init__.py
+++ src/persil/__init__.py
@@ -1,5 +1,6 @@
 from .simplexchain import *
 from .homology import *
 from .graphical import *
+from .vietorisrips import *
 
-__all__ = ["simplexchain","homology","graphical"]
+__all__ = ["simplexchain","homology","graphical","vietorisrips","Simplex","FilteredComplex","ZomorodianCarlsson","persistence_diagram","barcode","RipsComplex"]
