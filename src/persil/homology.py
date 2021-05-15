44da37786e0a9f6306ab363e6004590a3d617864
Guillaume Rousseau <=>
2021-05-15 14:26:03 +0200

    code cleanup

--- src/persil/homology.py
+++ src/persil/homology.py
@@ -9,8 +9,8 @@ class FilteredComplex:
     # the degree of a simplex is the lowest index for which it appears in the complex
 
     def __init__(self,warnings = False):
-        self._simplices = [] # list of simplices, ordered by dimension, then by degree, then by an arbitrary predefined order to break ties
-        self._degrees_dict = {} # also contains the degrees. keys are simplices
+        self._simplices = [] # list of simplices
+        self._degrees_dict = {} # contains the degrees. keys are simplices
         self._numSimplices = 0
         self._dimension = 0
         self._warnings = warnings
@@ -72,31 +72,47 @@ class FilteredComplex:
 
 
 class ZomorodianCarlsson:
-    def __init__(self,filteredComplex,field = 2,strict = False,verbose = False):
-        self.n = filteredComplex._numSimplices
+    def __init__(self,filteredComplex,field = 2,strict = True,verbose = False):
+        """
+        Class for Zomorodian and Carlsson's algorithm for persistent homology.
+        Initialization does not compute homology. Call self.computeIntervals
+        for the actual computation.
+
+        Arguments:
+        - filteredComplex should be an instance of FilteredComplex, on which
+        homology will be computed.
+        - field: prime number, specifies the field over which homology is
+        computed. Default value: 2
+        - strict: Boolean. If set to True, homology elements of duration zero,
+        i.e. intervals of the form (x,x), will be ignored. Default value: True
+        - verbose: Boolean. If set to True, the computeIntervals method will
+        output its progress throughout the algo. Default value: False
+
+        """
+
+        self.numSimplices = filteredComplex._numSimplices
+
+        # first, order the simplices in lexico order on dimension, degree and then arbitrary order
         def key(s):
             d = filteredComplex.degree(s)
             return (s.dim,d,s)
         filteredComplex._simplices.sort(key = key)
 
         self.simplices = filteredComplex._simplices[:]
-        self._indexBySimplex = {}
-        for i in range(self.n):
-            self._indexBySimplex[self.simplices[i]] = i
         self.dim = filteredComplex._dimension
         self.degrees = filteredComplex._degrees_dict.copy()
         self.field = field
 
 
-        self.marked = [False for i in range(self.n)]
-        self.T = [None for i in range(self.n)] # contains couples (simplex,chain)
-        self.intervals = [[] for i in range(self.dim+1)]
+        self.marked = [False for i in range(self.numSimplices)]
+        self.T = [None for i in range(self.numSimplices)] # contains couples (index,chain)
+        self.intervals = [[] for i in range(self.dim+1)] # contains homology intervals once the algo has finished
         self.pairs = []
 
         self._maxDeg = filteredComplex._maxDeg
         self._strict = strict
-
-        self.verbose = verbose
+        self._verbose = verbose
+        self._homologyComputed = False
 
     def addInterval(self,k,t,s):
         i = self.degrees[t]
@@ -106,7 +122,7 @@ class ZomorodianCarlsson:
             j = self.degrees[s]
 
         if i != j or (not self._strict):
-            #if self.verbose:
+            #if self._verbose:
                 #print("Adding {}-interval ({},{})".format(k,i,j))
             self.intervals[k].append((i,j))
             self.pairs.append((t,s))
@@ -116,19 +132,27 @@ class ZomorodianCarlsson:
 
 
     def computeIntervals(self):
-        if self.verbose:
+        """
+        Computes the homology group. After running this method,
+        homology intervals become accessible with self.getIntervals(k).
+
+        """
+        if self._homologyComputed:
+            print("Homology was already computed.")
+            return
+        if self._verbose:
             print("Beginning first pass")
-        for j in range(self.n):
-            if j%1000 == 0 and self.verbose:
-                print('{}/{}'.format(j,self.n))
+        for j in range(self.numSimplices):
+            if j%1000 == 0 and self._verbose:
+                print('{}/{}'.format(j,self.numSimplices))
             s = self.simplices[j]
-            #if self.verbose:
+            #if self._verbose:
                 #print("Examining {}. Removing pivot rows...".format(s))
             d = self.removePivotRows(s)
-            #if self.verbose:
+            #if self._verbose:
                 #print("Done removing pivot rows")
             if d.isEmpty():
-                #if self.verbose:
+                #if self._verbose:
                     #print("Boundary is empty when pivots are removed: marking {}".format(s))
                 self.marked[j] = True
             else:
@@ -138,23 +162,24 @@ class ZomorodianCarlsson:
                 k = t.dim-1
                 self.T[maxInd] = (s,d)
                 self.addInterval(k,t,s)
-                #if self.verbose:
+                #if self._verbose:
                     #print("Boundary non-reducible: T{} is set to:".format(t))
                     #print(str(d))
 
-        if self.verbose:
+        if self._verbose:
             print("First pass over, beginning second pass")
-        for j in range(self.n):
-            if j%1000 == 0 and self.verbose:
-                print('{}/{}'.format(j,self.n))
+        for j in range(self.numSimplices):
+            if j%1000 == 0 and self._verbose:
+                print('{}/{}'.format(j,self.numSimplices))
             s = self.simplices[j]
             if self.marked[j] and not self.T[j]:
                 k = s.dim -1
-                #if self.verbose:
+                #if self._verbose:
                     #print("Infinite interval found for {}.".format(s))
                 self.addInterval(k,s,None)
-        if self.verbose:
+        if self._verbose:
             print("Second pass over")
+        self._homologyComputed = True
 
 
 
@@ -166,23 +191,23 @@ class ZomorodianCarlsson:
         d.purge()
         while not d.isEmpty():
 
-            #if self.verbose:
+            #if self._verbose:
                 #print("Current chain d:")
                 #print(str(d))
 
             maxInd = self.maxIndex(d)
             t = self.simplices[maxInd]
-            #if self.verbose:
+            #if self._verbose:
                 #print("simplex with max index in d: {} with index {}".format(maxInd))
 
             if not self.T[maxInd]:
-                #if self.verbose:
+                #if self._verbose:
                     #print("{} is not in T: done removing pivot rows".format(t))
                 break
 
             c = self.T[maxInd][1]
             q = c.getCoeff(t)
-            #if self.verbose:
+            #if self._verbose:
                 #print("{} is in T with coeff {}: ".format(t,q),"##########",str(c),"##########",sep='\n'    )
             d = d - pow(q,self.field-2,self.field)*c
         return d
@@ -198,10 +223,18 @@ class ZomorodianCarlsson:
 
 
     def getIntervals(self,d):
+        """
+        Returns the list of d-dimensional homology elements.
+        Can only be run after computeIntervals has been run.
+        """
+        if not self._homologyComputed:
+            print("Warning: homology has not yet been computed. This will return an empty list.")
         return self.intervals[d][:]
 
     def bettiNumber(self,k,l,p):
         res = 0
+        if not self._homologyComputed:
+            print("Warning: homology has not yet been computed. This will return 0.")
         for (i,j) in self.intervals[k]:
             if (i <= l and l + p < j ) and p>=0:
                     res+=1
